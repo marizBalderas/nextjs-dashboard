f164d5441d40424377aeb833180e81691b343967
Mariz A. Balderas <marixbalderas@gmail.com>
2025-11-29 10:53:48 +0800

    chapter 12

--- app/lib/data.ts
+++ app/lib/data.ts
@@ -116,6 +116,7 @@ export async function fetchFilteredInvoices(
       LIMIT ${ITEMS_PER_PAGE} OFFSET ${offset}
     `;
 
+     
     return invoices;
   } catch (error) {
     console.error('Database Error:', error);
@@ -162,6 +163,7 @@ export async function fetchInvoiceById(id: string) {
       amount: invoice.amount / 100,
     }));
 
+    console.log(invoice); // Invoice is an empty array []
     return invoice[0];
   } catch (error) {
     console.error('Database Error:', error);
